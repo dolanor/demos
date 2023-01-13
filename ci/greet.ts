42331888d99c0e890d1b75f4f333d1629fbccfa3
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2023-01-13 15:48:26 +0100

    update cli for dagger Go SDK v0.3.1

--- ci/greet.ts
+++ ci/greet.ts
@@ -2,28 +2,27 @@ import Client, { connect } from "@dagger.io/dagger"
 
  // initialize Dagger client
 connect(async (client: Client) => {
-	let go = await client
+	const go = await client
 		.container()
 		.from("golang:1.19")
 
-	let srcId = await client
+	const src = await client
 		.host()
-		.workdir([".gitignore", "node_modules", "build.ts"])
-		.id()
+		.directory(".", { exclude: [".gitignore", "node_modules", "build.ts"] })
 
-	let goarches = ["amd64", "arm64"]
-	let geese = ["windows", "darwin", "linux"]
+	const goarches = ["amd64", "arm64"]
+	const geese = ["windows", "darwin", "linux"]
 
 	await Promise.all(goarches.map(async (goarch) => {
 		await Promise.all(geese.map(async (goos) => {
 			await go
-				.withMountedDirectory("/app", srcId.id)
-				.withWorkdir("/app")
+				.withMountedDirectory("/app", src)
+				.withWorkdir("/app/build")
 				.withEnvVariable("GOOS", goos)
 				.withEnvVariable("GOARCH", goarch)
-				.exec(["go", "build", "-o", `greetings`])
-				.file("greetings")
-				.export(`build/${goos}/${goarch}/dagger-greetings`)
+				.withExec(["go", "build", "../cmd/greet"])
+				.directory(".")
+				.export(`build/${goos}/${goarch}/`)
 		}))
 	}))
 }, {LogOutput: process.stdout});
